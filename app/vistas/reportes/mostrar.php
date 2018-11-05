6b3c407b77c7a5b4bb9cce8a0226a2f6981c257e
Juanqui Mulki <juanquimulki@hotmail.com>
2018-11-05 19:40:52 -0300

    Calculo de kms/hrs por trabajo

--- app/vistas/reportes/mostrar.php
+++ app/vistas/reportes/mostrar.php
@@ -7,6 +7,7 @@
     <table class="table table-condensed">
       <tr>
         <th style="width: 50px">#</th>
+        <th style="width: 100px">Fecha</th>
         <th style="width: 100px">Tarea</th>
         <th style="width: 100px">Litros</th>
         <th style="width: 150px">Odómetro/Reloj</th>
@@ -14,23 +15,28 @@
         <th>Totales</th>
       </tr>
       <tr>
-        <td></td><td><span class='badge bg-blue'>Iniciales</span></td><td></td><td><?php echo $inicial; ?></td>
+        <td></td><td></td><td><span class='badge bg-blue'>Iniciales</span></td><td></td><td><?php echo $inicial; ?></td>
       </tr>
       <?php 
       $i=1;
+      $cuenta = $inicial;
       while ($registro = $reporte->fetch()) {
         echo "<tr>";
         echo "<td>$i</td>";
+        echo "<td>".$registro['fecha']."</td>";
         switch ($registro['tipo']) {
           case 1:
             echo "<td><span class='badge bg-green'>Carga</span></td>";
             echo "<td>".$registro['valor']."</td>";
+            echo "<td></td>";
             echo "<td></td>";
             break;
           case 2:
             echo "<td><span class='badge bg-red'>Trabajo</span></td>";
             echo "<td></td>";
             echo "<td>".$registro['valor']."</td>";
+            echo "<td>".($registro['valor']-$cuenta)."</td>";
+            $cuenta = $registro['valor'];
             break;
         }
         
